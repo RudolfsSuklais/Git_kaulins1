5bd4f753804354ef3aa938bd1880e6045b3d227a
Šuklais <rudolfs.suklais@gmail.com>
2024-04-16 11:53:39 +0300

    Ieviesta metode

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -2,10 +2,18 @@ import java.util.Random;
 public class Kaulins {
 
 	public static void main(String[] args) {
-     int skaitlis;
-     Random rand = new Random();
-     skaitlis = rand.nextInt(6)+1;
-     System.out.println("Tu uzmeti: "+skaitlis);
+		mestKaulinu();
+	}
+	
+		static void mestKaulinu() {
+			 int skaitlis;
+		     Random rand = new Random();
+		     skaitlis = rand.nextInt(6)+1;
+		     System.out.println("Tu uzmeti: "+skaitlis);
+		
+		
+		
+     
      
 		
 		
